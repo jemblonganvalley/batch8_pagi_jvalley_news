61033e9c8abb9a662e642c41932f6c89b478d484
jemblonganvalley <jemblonganvalley@gmail.com>
2021-03-30 10:51:42 +0700

    up

--- routes/home_routes.js
+++ routes/home_routes.js
@@ -1,9 +1,33 @@
 //import package yang dibutuhkan
 const express = require("express");
 const home = express.Router();
+const axios = require("axios");
 
 home.get("/", (req, res) => {
-  res.render("home.html");
+  axios
+    .get("https://dev.to/api/articles?tag=nextjs&page=2&per_page=9&state=fresh")
+    .then((e) => {
+      res.render("home", { data: e.data });
+      console.log(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+home.get("/home/:page", (req, res) => {
+  const page = req.params.page;
+  axios
+    .get(
+      `https://dev.to/api/articles?tag=nextjs&page=${page}&per_page=9&state=fresh`
+    )
+    .then((e) => {
+      res.render("home", { data: e.data });
+      console.log(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
 module.exports = home;
