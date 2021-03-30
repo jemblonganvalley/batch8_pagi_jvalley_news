e7f965237a190be515649ddd186c08f9ca21205f
jemblonganvalley <jemblonganvalley@gmail.com>
2021-03-30 10:54:17 +0700

    upload

--- routes/home_routes.js
+++ routes/home_routes.js
@@ -4,30 +4,7 @@ const home = express.Router();
 const axios = require("axios");
 
 home.get("/", (req, res) => {
-  axios
-    .get("https://dev.to/api/articles?tag=nextjs&page=2&per_page=9&state=fresh")
-    .then((e) => {
-      res.render("home", { data: e.data });
-      console.log(data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-});
-
-home.get("/home/:page", (req, res) => {
-  const page = req.params.page;
-  axios
-    .get(
-      `https://dev.to/api/articles?tag=nextjs&page=${page}&per_page=9&state=fresh`
-    )
-    .then((e) => {
-      res.render("home", { data: e.data });
-      console.log(data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  res.render("home");
 });
 
 module.exports = home;
